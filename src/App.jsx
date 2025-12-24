0ed445d13daee381d484360e3904a016d2a23632
shivamdayma <shivamdayma19@gmail.com>
2025-12-24 17:57:48 +0530

    now it is mobile compatable

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <div className=" w-screen bg-[radial-gradient(circle_at_center,_#2f2f2f_0%,_#1f1f1f_30%,_#000000_70%)]">
+    <div className="w-screen px-4 sm:px-6 lg:px-12 bg-[radial-gradient(circle_at_center,_#2f2f2f_0%,_#1f1f1f_30%,_#000000_70%)]">
       <Header />
       <About />
       <Skills />
